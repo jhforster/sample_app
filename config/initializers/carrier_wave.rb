5bc78de51ad481d545ecc746da7d584a04d0de3a
jhforster <jhforster@gmail.com>
2017-01-16 13:57:24 -0500

    Update credential info

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,14 +3,9 @@ if Rails.env.production?
     config.fog_credentials = {
       # Configuration for Amazon S3
       :provider               => 'AWS',
-      :aws_access_key_id      => ENV['AKIAI7LKKKGSNQ4AZCHA'],
-      :aws_secret_access_key  => ENV['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']
+      :aws_access_key_id      => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key  => ENV['S3_SECRET_KEY']
     }
     config.fog_directory      = ENV['jhforstersampleapp']
   end
 end
-
-
-
-# AKIAIG4ZNVBXVQYJR7IA
-# h+PHy6TLVKg4HGFoG4l1JWdlMpqgfsFOvjgJDE/H
